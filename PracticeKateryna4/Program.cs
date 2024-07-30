97aa07fff423d4b15122e0ed63156bf05b272692
kkarpenkooo <ivanyshevich@gmail.com>
2024-07-30 22:47:17 +0300

    Add files via upload

---
+++ PracticeKateryna4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        Console.WriteLine("Введіть натуральне число n:");
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int q = 1; q < n; q++)
+        {
+            if ((n - q) % (q + 1) == 0)
+            {
+                int m = (n - q) / (q + 1);
+                if (m > 0)
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine($"Кількість рівних дільників числа {n}: {count}");
+    }
+}
