2e0bf6aa79fe020c816c2c4b62c36f33c9fdc5ca
kkarpenkooo <ivanyshevich@gmail.com>
2024-07-30 22:48:07 +0300

    Add files via upload

---
+++ PracticeKateryna6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the coordinates of the vector:");
+        string[] input = Console.ReadLine().Split();
+
+        int x1 = int.Parse(input[0]);
+        int y1 = int.Parse(input[1]);
+        int x2 = int.Parse(input[2]);
+        int y2 = int.Parse(input[3]);
+
+        double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        Console.WriteLine($"{length:F6}");
+    }
+}
