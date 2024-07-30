14e9cf6764b662253136f455647553d68c2af3d5
kkarpenkooo <ivanyshevich@gmail.com>
2024-07-30 22:49:25 +0300

    Add files via upload

---
+++ PracticeKateryna9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Input reading
+        Console.WriteLine("Enter values for x and y:");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        // Calculate the minimum number of steps
+        int steps = MinimumSteps(x, y);
+        Console.WriteLine($"Minimum number of steps from {x} to {y}: {steps}");
+    }
+
+    static int MinimumSteps(int x, int y)
+    {
+        int distance = y - x;
+        int stepLength = 1;
+        int totalSteps = 0;
+
+        while (distance > 0)
+        {
+            totalSteps++;
+            distance -= stepLength;
+
+            // Increase stepLength if the remaining distance allows it
+            if (distance >= stepLength + 1)
+            {
+                stepLength++;
+            }
+        }
+
+        return totalSteps;
+    }
+}
